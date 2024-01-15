1bc80bdca7197142d9dbb04c84be7c0be2142d15
Everrick Wright <ew02726@georgiasouthern.edu>
2024-01-14 19:35:12 -0500

    fixing fib.ts

--- src/fib.ts
+++ src/fib.ts
@@ -1,12 +1,19 @@
 // util function that computes the fibonacci numbers
 export default function fibonacci(n) {
-  if (n < 0) {
-    return -1;
-  } else if (n == 0) {
-    return 0;
-  } else if (n == 1) {
-    return 1;
-  }
+    if (n < 0) {
+        var x = -1;
+        return x;
+    }
+    else if (n == 0) {
+        var y = 0;
+        return 0;
+    }
+    else if (n == 1) {
+        var z = 1;
+        return 1;
 
-  return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
+
